111bf043204618df92c8654edc0177b4836b1709
TejasAI31 <tejeshwarsingh1205@gmail.com>
2024-07-11 15:56:27 +0530

    initialised project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nss_project/main.dart';
+import 'package:nss_project/app_entry.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
