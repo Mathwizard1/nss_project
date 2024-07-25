a281b9f2f35dd5b0fa1b52aecf8bd119947393ff
ewilipsic <bansalayush2006@gmail.com>
2024-07-25 23:52:09 +0530

    no more stream builder

--- lib/picprofile_page.dart
+++ lib/picprofile_page.dart
@@ -21,7 +21,7 @@ class _PicProfilePageState extends State<PicProfilePage> {
 
   Future<void> _pickImage(ImageSource source) async {
     XFile? selected = await ImagePicker().pickImage(source: source);
-    
+
     setState(() {
       _imagefile = selected;
     });
@@ -115,57 +115,61 @@ class _PicProfilePageState extends State<PicProfilePage> {
               ),
             ),
             Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                    decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        label: Text(
-                          'Change Name',
-                          style: TextStyle(fontSize: 17.0),
-                        )),
-                    onChanged: (text) {
-                      name = text;
-                      },
-                    ),
-                    TextButton(onPressed: (){Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const Configurables()));}, child: Text('Change Configurables',style: TextStyle(decoration: TextDecoration.underline),)),
-                    // Padding(
-                    //   padding: const EdgeInsets.all(5.0),
-                    //   child: SizedBox(
-                    //         width: double.infinity,
-                    //         child: ElevatedButton(
-                    //           style: ElevatedButton.styleFrom(
-                    //             foregroundColor: Colors.white, 
-                    //             backgroundColor: Colors.blue[900],
-                    //           ),
-                    //           onPressed: (){
-                    //             Navigator.push(context,MaterialPageRoute(builder: (context)=>LeaderboardPage()));
-                    //           },
-                    //           child: const Text(
-                    //             'Leaderboard',
-                    //             style: TextStyle(
-                    //               fontSize: 16,
-                    //               fontWeight: FontWeight.bold,
-                    //             ),
-                    //           ),
-                    //         ),
-                    //       ),
-                    // ),
-                    ]
-                  ),
-              ),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+              child: Column(children: <Widget>[
+                TextField(
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      label: Text(
+                        'Change Name',
+                        style: TextStyle(fontSize: 17.0),
+                      )),
+                  onChanged: (text) {
+                    name = text;
+                  },
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Configurables()));
+                    },
+                    child: const Text(
+                      'Change Configurables',
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    )),
+                // Padding(
+                //   padding: const EdgeInsets.all(5.0),
+                //   child: SizedBox(
+                //         width: double.infinity,
+                //         child: ElevatedButton(
+                //           style: ElevatedButton.styleFrom(
+                //             foregroundColor: Colors.white,
+                //             backgroundColor: Colors.blue[900],
+                //           ),
+                //           onPressed: (){
+                //             Navigator.push(context,MaterialPageRoute(builder: (context)=>LeaderboardPage()));
+                //           },
+                //           child: const Text(
+                //             'Leaderboard',
+                //             style: TextStyle(
+                //               fontSize: 16,
+                //               fontWeight: FontWeight.bold,
+                //             ),
+                //           ),
+                //         ),
+                //       ),
+                // ),
+              ]),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
 
 class Configurables extends StatefulWidget {
   const Configurables({super.key});
@@ -175,69 +179,139 @@ class Configurables extends StatefulWidget {
 }
 
 class _ConfigurablesState extends State<Configurables> {
+ 
+  bool isHoursChanged = false;
+  bool isWingsChanged = false;
 
-  
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.sizeOf(context).height;
-    bool isHoursChanged = false;
-    bool isWingsChanged = false;
-    
-    
-    late int totalhours;
 
+    late int totalhours;
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        if(isHoursChanged){
-          FirebaseFirestore.instance.collection('configurables').doc('documen').set({'mandatory-hours' : totalhours},SetOptions(merge: true));
-        }
-      },),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (isHoursChanged) {
+            FirebaseFirestore.instance
+                .collection('configurables')
+                .doc('documen')
+                .set({'mandatory-hours': totalhours}, SetOptions(merge: true));
+          }
+        },
+      ),
       body: SafeArea(
         child: Column(
-
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: StreamBuilder(
-                stream: FirebaseFirestore.instance.collection('configurables').doc('document').snapshots(),
-                builder: (context, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.active){
-                  totalhours = snapshot.data!['mandatory-hours'];
-                  List<String> wings = snapshot.data!['wings'].map<String>((wing){String ret = wing; return ret;}).toList();
-                  return Container(
-                    height: height/1.5,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        TextField(
-                                  decoration: const InputDecoration(
-                                      border: UnderlineInputBorder(),
-                                      label: Text(
-                                        'Change Total Hours Required',
-                                        style: TextStyle(fontSize: 17.0),
-                                      )),
-                                  keyboardType: TextInputType.number,
-                                  inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
-                                  onChanged: (text) {
-                                    totalhours = int.parse(text);
-                                    isHoursChanged = true;
-                                  },
-                                  ),
-                        const SizedBox(height: 20,),
-                        const Text('wings'),
-                        Flexible(
-                          child: WingList(wings: wings),
-                        )
-                      ],
-                    ),
-                  );
-                  }
-                  return const Text("No connection");
-                }
-              ),
+                  stream: FirebaseFirestore.instance
+                      .collection('configurables')
+                      .doc('documen')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      totalhours = snapshot.data!['mandatory-hours'];
+                      List<String> wings =
+                          snapshot.data!['wings'].map<String>((wing) {
+                        String ret = wing;
+                        return ret;
+                      }).toList();
+                      return Container(
+                        height: height / 1.5,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextField(
+                              decoration: const InputDecoration(
+                                  border: UnderlineInputBorder(),
+                                  label: Text(
+                                    'Change Total Hours Required',
+                                    style: TextStyle(fontSize: 17.0),
+                                  )),
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              onChanged: (text) {
+                                totalhours = int.parse(text);
+                                isHoursChanged = true;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text('wings'),
+                            Flexible(
+                              child: ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: wings.length + 1,
+                                  itemBuilder: (context, index) {
+                                    String? newWing;
+                                    if (index == wings.length) {
+                                      return TextButton(
+                                          onPressed: () => showDialog(
+                                            context: context,
+                                            builder:(BuildContext context){
+                                              return AlertDialog(
+                                                title: const Text('Name of new wing'),
+                                                content: TextField(
+                                                  onChanged: (text){newWing = text;},
+                                                ),
+                                                actions: [
+                                                  TextButton(onPressed: (){
+                                                    if(newWing != null){
+                                                      wings.add(newWing ?? '');
+                                                      FirebaseFirestore.instance.collection('configurables').doc('documen').set({'wings': wings}, SetOptions(merge: true));
+                                                      setState(() {});
+                                                    }
+                                                    Navigator.pop(context);
+
+                                                 
+
+                                                  }, child: const Text('confirm'),)
+                                                ],
+                                              );
+                                            }
+                                          ),
+                                          child: const Text('add event'));
+                                    }
+                                    return Card(
+                                      child: ListTile(
+                                        title: Text(wings[index]),
+                                        trailing: IconButton(
+                                          icon: const Icon(Icons.delete),
+                                          onPressed: () =>
+                                            showDialog(
+                                              context: context,
+                                              builder: (BuildContext context){
+                                               return AlertDialog(
+                                                title: const Text('Do you want to delete this wing'),
+                                                actions: [
+                                                  TextButton(onPressed: (){Navigator.pop(context);}, child: const  Text('No')),
+                                                  TextButton(onPressed: (){
+                                                    wings.removeAt(index);
+                                                    FirebaseFirestore.instance.collection('configurables').doc('documen').set({'wings': wings}, SetOptions(merge: true));
+                                                    setState(() {});
+                                                    Navigator.pop(context);
+                                                  }, child: const Text('Yes')),
+                                              
+                                                ],
+                                              );},
+                                            ),
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                    return const Text("No connection");
+                  }),
             ),
-        
           ],
         ),
       ),
@@ -245,37 +319,3 @@ class _ConfigurablesState extends State<Configurables> {
   }
 }
 
-class WingList extends StatefulWidget {
-  const WingList({
-    super.key,
-    required this.wings,
-  });
-
-  final List<String> wings;
-
-  @override
-  State<WingList> createState() => _WingListState();
-}
-
-class _WingListState extends State<WingList> {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: widget.wings.length + 1,
-      itemBuilder: (context,index){
-        if(index == widget.wings.length){
-          return TextButton(onPressed: (){}, child: const Text('add event'));
-        }
-        return Card(
-          child: ListTile(
-            title: Text(widget.wings[index]),
-            trailing: IconButton(icon: const Icon(Icons.delete),onPressed: (){setState(() {
-              widget.wings.removeAt(index);
-            });},),
-          ),
-    
-        );
-      });
-  }
-}
